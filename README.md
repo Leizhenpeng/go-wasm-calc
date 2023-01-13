5c5957d4d2235711286b926edfc6a49ea93fdefb
river <laolei@forkway.cn>
2023-01-13 15:51:48 +0800

    init: init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# go-wasm-calc
+> A simple calculator written in TinyGo and compiled to WebAssembly
+![img.png](doc/img.png)
+
+
+## Features
+- [Wasm](https://webassembly.org/)
+- [TinyGo](https://tinygo.org/)
+- [Vite](https://cn.vitejs.dev/guide/#overview)
+- [Vue](https://cn.vuejs.org/)
---
+++ src/components/Calc.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+const expression = ref('2023-2022')
+
+watch(expression, (newVal, oldVal) => {
+  console.log('newVal', newVal)
+  console.log('oldVal', oldVal)
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div class="card">
+      <input type="text" class="format" v-model="expression" style="text-align: center; margin-top: 10px " />
+      <button type="button" class="format" @click="count++" style="margin-top: 10px; ">Result Is {{  }}</button>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
